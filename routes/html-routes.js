3f7a9369b0699ae2b02e6b4ce4603bc91d2c3b4e
David Harris <jdh.programmer@gmail.com>
2020-12-23 14:37:58 -0600

    new html routes file name

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,35 +1,13 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
+// html-routes sending users to the apps html pages //
 
-// Dependencies
-// =============================================================
-var path = require("path");
+const path = require("path");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    app.get("/", (req, res) => res.sendFile(path.join(__dirname, "..public/index.html")));
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
+    app.get("/excercise", (req, res) => res.sendFile(path.join(__dirname, "..public/excercise.html")));
 
-  // cms route loads cms.html
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
-  });
-
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-  });
+    app.get("/stats", (req, res) => res.sendFile(path.join(__dirname, "..public/stats.html")));
 
 };
