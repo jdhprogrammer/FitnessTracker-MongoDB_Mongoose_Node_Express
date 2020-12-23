ce945a73308d66ebf41469b1c819a74bc5c96af2
David Harris <jdh.programmer@gmail.com>
2020-12-23 14:23:03 -0600

    wrote first attempt at custom total duration function

--- models/workout.js
+++ models/workout.js
@@ -1,53 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        required: true,
     },
 
     excercise: [{
         type: {
             type: String,
-            required: true
+            required: true,
         },
         name: {
             type: String,
             trim: true,
             lowercase: true,
-            required: true
+            required: true,
         },
         duration: {
             type: Number,
             trim: true,
-            required: true
         },
         weight: {
             type: Number,
             trim: true,
-            required: true
         },
         reps: {
             type: Number,
             trim: true,
-            required: true
         },
         sets: {
             type: Number,
             trim: true,
-            required: true
-        }
-    }]
+        },
+        distance: {
+            type: Number,
+            trim: true,
+        },
+    }, ],
+}, {
+    toJSON: {
+        // include any virtual properties when data is requested
+        virtuals: true,
+    },
 });
 
 // adds a dynamically-created property to schema
-workoutSchema.virtual('totalDuration').get(function() {
+workoutSchema.virtual("totalDuration").get(function() {
     //loop over this.excercises and return the total duration
-
+    this.excercises.aggregate({
+        $addFields: {
+            totalDuration: {
+                $sum: "$duration",
+            },
+        },
+    });
 });
 
-const Workout = mongoose.model('Workout', workoutSchema);
+workoutSchema.virtual();
+
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
