fc87437cfd345181f8189ef1a58bfa097c138252
David Harris <jdh.programmer@gmail.com>
2020-12-23 22:52:26 -0600

    was still exporting function(app) on html routes page, instead of exporting router

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,21 +1,17 @@
-// html-routes sending users to the apps html pages //
-
 const path = require("path");
 const express = require("express");
 const router = express.Router();
 
-module.exports = function(app) {
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-    router.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"))
-    });
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
-    router.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"))
-    });
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/html.html"));
+});
 
-    router.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"))
-    });
-
-};
+module.exports = router;
