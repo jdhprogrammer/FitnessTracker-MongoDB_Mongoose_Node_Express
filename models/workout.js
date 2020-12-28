e69acd9e90b3a0adcc1df35152320300437c45c6
David Harris <jdh.programmer@gmail.com>
2020-12-28 14:05:29 -0600

    changed some of the colors on the app. deleted commented out sections. cleaned up code.

--- models/workout.js
+++ models/workout.js
@@ -42,9 +42,6 @@ const WorkoutSchema = new Schema({
         },
     }, ],
 }, {
-    toObject: {
-        virtuals: true,
-    },
     toJSON: {
         virtuals: true,
     },
@@ -59,82 +56,3 @@ WorkoutSchema.virtual("totalDuration").get(function() {
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
-
-
-
-
-
-// const mongoose = require("mongoose");
-
-// const Schema = mongoose.Schema;
-
-// const workoutSchema = new Schema({
-//     day: {
-//         type: Date,
-//         default: Date.now,
-//         required: true,
-//     },
-
-//     excercise: [{
-//         type: {
-//             type: String,
-//             required: true,
-//         },
-//         name: {
-//             type: String,
-//             trim: true,
-//             lowercase: true,
-//             required: true,
-//         },
-//         duration: {
-//             type: Number,
-//             trim: true,
-//         },
-//         weight: {
-//             type: Number,
-//             trim: true,
-//         },
-//         reps: {
-//             type: Number,
-//             trim: true,
-//         },
-//         sets: {
-//             type: Number,
-//             trim: true,
-//         },
-//         distance: {
-//             type: Number,
-//             trim: true,
-//         },
-//     }, ],
-// }, {
-//     toObject: {
-//         virtuals: true,
-//     },
-
-//     toJSON: {
-//         // include any virtual properties when data is requested
-//         virtuals: true,
-//     },
-// });
-
-// workoutSchema.virtual("totalDuration").get(function() {
-//     return this.exercises.reduce((total, exercise) => {
-//         return total + (exercise.duration || 0);
-//     }, 0);
-// });
-// adds a dynamically-created property to schema
-// workoutSchema.virtual("totalDuration").get(function() {
-//     //loop over this.excercises and return the total duration
-//     this.excercises.aggregate({
-//         $addFields: {
-//             totalDuration: {
-//                 $sum: "$duration",
-//             },
-//         },
-//     });
-// });
-
-// const Workout = mongoose.model("Workout", workoutSchema);
-
-// module.exports = Workout;
